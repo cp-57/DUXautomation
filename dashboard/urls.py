4ad9346df15c15549541733a8ec5e6a609e1804e
cp-57 <109479938+cp-57@users.noreply.github.com>
2024-05-23 17:59:57 -0700

    big imporvements, put in real folders

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', onboarding_list, name='onboarding-list'),
     path('onboarding/new/', create_onboarding, name='create-onboarding'),
     path('onboarding/<uuid:id>/', onboarding_detail, name='onboarding-detail'),
-    path('dashboard-step-two/', dashboard_step_two_view, name='dashboard-step-two'),
-    path('dashboard-step-three/', dashboard_step_three_view, name='dashboard-step-three'),
+    path('dashboard-step-two/<uuid:id>/', dashboard_step_two_view, name='dashboard-step-two'),
+    path('dashboard-step-three/<uuid:id>/', dashboard_step_three_view, name='dashboard-step-three'),
     path('dashboard-step-four/', dashboard_step_four_view, name='dashboard-step-four'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -7,6 +7,10 @@ import os
 from django.contrib.auth.decorators import login_required
 from .utils import get_google_credentials
 from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload, MediaInMemoryUpload
+import io
+import os
+from googleapiclient.errors import HttpError
 
 from .forms import *
 
@@ -75,74 +79,112 @@ def onboarding_detail(request, id):
     return render(request, 'dashboard.html', {'onboarding_object': onboarding_object,'dashboard_step_one_form': dashboard_step_one_form})
 
 
-def dashboard_step_two_view(request):
+# owner onboarding folder - 1ub8g1ll4G0cNYb8OkqbJjwDIrRPHX5-7
+# properties folder - 141hQMIqpuAXcwzvAczhiClzFvL-SGemb
+# expenses folder - 1LRJmLgbAS339l9Q3IB-iEu0rA-yADlMy
+
+def dashboard_step_two_view(request, id):
     if request.method=="POST":
+        onboarding_object = get_object_or_404(OnboardingObject, id=id)
         # Assume you save the data here
-        creds = get_google_credentials(request)
+        creds = get_google_credentials(request.user)
 
         # Build the Drive v3 API service
         service = build('drive', 'v3', credentials=creds)
 
-        # Parent folder ID - ONBOARDING_AUTOMATION
-        parent_folder_id = request.session['property_folder_id']
+        # acquire address and owner info from object
+        address = onboarding_object.address
+        owner = onboarding_object.owner 
 
-        owner = request.session['owner'] 
-        address = request.session['address']
+        # OWNER FOLDER
+        parent_folder_id = '1ub8g1ll4G0cNYb8OkqbJjwDIrRPHX5-7'
 
-        folders_to_create = ['Owner','Property','Expenses']
-
-        for folder_name in folders_to_create:
-            # Define the folder metadata
-            folder_metadata = {
-                'name': f'{address}_{owner}_{folder_name}',
+        folder_metadata = {
+                'name': owner,
                 'mimeType': 'application/vnd.google-apps.folder',
                 'parents': [parent_folder_id]  
             }
 
-            # Create the folder inside the specified parent folder
-            folder = service.files().create(body=folder_metadata, fields='id').execute()
+        # Create the folder inside the specified parent folder
+        folder = service.files().create(body=folder_metadata, fields='id').execute()
 
-            if folder_name=="Owner":
-                request.session['owner_folder_id'] = folder.get('id')
-        
-        return redirect('dashboard')
+        onboarding_object.owner_folder_id = folder.get('id')
 
-def dashboard_step_three_view(request):
+        # PROPERTY FOLDER
+        parent_folder_id = '141hQMIqpuAXcwzvAczhiClzFvL-SGemb'
+
+        folder_metadata = {
+                'name': address,
+                'mimeType': 'application/vnd.google-apps.folder',
+                'parents': [parent_folder_id]  
+            }
+
+        # Create the folder inside the specified parent folder
+        folder = service.files().create(body=folder_metadata, fields='id').execute()
+
+        onboarding_object.property_folder_id = folder.get('id')
+
+        # EXPENSES FOLDER
+        parent_folder_id = '1LRJmLgbAS339l9Q3IB-iEu0rA-yADlMy'
+
+        folder_metadata = {
+                'name': f'{address} [Expenses]',
+                'mimeType': 'application/vnd.google-apps.folder',
+                'parents': [parent_folder_id]  
+            }
+
+        # Create the folder inside the specified parent folder
+        folder = service.files().create(body=folder_metadata, fields='id').execute()
+
+        onboarding_object.expenses_folder_id = folder.get('id')
+
+        onboarding_object.save()
+
+        url = reverse('onboarding-detail', kwargs={'id': id})
+
+        return redirect(url)
+
+def dashboard_step_three_view(request, id):
     if request.method=="POST":
-        creds = get_google_credentials(request)
-        service = build('drive', 'v3', credentials=creds)
+        onboarding_object = get_object_or_404(OnboardingObject, id=id)
+        creds = get_google_credentials(request.user)
+        drive_service = build('drive', 'v3', credentials=creds)
+        forms_service = build('forms', 'v1', credentials=creds)
 
-        owner_folder = request.session['owner_folder_id']
-
-
-        bank_information_request = '1hOZUQ3r1dPi6_nOrHZTioQ4MlT8YXGox'
-        service_agreement = '1RCPM2ReSEVa-TXLZs969sbWXj0gstozV'
-        rental_agreement = '17uvFKKkvTS5a2lvrwYt-pc87Snxb_cxk'
-        new_property_onboarding_template = '1u8I2DDT-2R-a0xXaRjTbAWo2VaAlzXvvm8yyQOs5bQk' #form
-        w9_template = '1v3qzTASmbviM4jNxOEW2KZ0I6kz5vDeW'
 
         new_copies_ids = []
 
-    document_ids = [bank_information_request,service_agreement,rental_agreement,new_property_onboarding_template,w9_template]
+        address = onboarding_object.address
+        owner = onboarding_object.owner 
 
-    for doc_id in document_ids:
-        copy_metadata = {
-            'parents': [owner_folder]  # Set the parent folder
-        }
 
-        # Create a copy of the document/form
-        copied_file = service.files().copy(
-            fileId=doc_id,
-            body=copy_metadata,
-            fields='id'
-        ).execute()
+        document_ids = {}
+        document_ids['Rental Agreement - '] = '1QYq1h4fH0FUzVTw02XEcNyDN35w9yjfLOduOS8mgXoQ'
+        document_ids['TEN PERCENT Destination UX Services Agreement - '] = '1ImNYdWTfAx6AuSmyRV8RaF3M3Nzly0zQ'
+        document_ids['W9 - '] = '1v3qzTASmbviM4jNxOEW2KZ0I6kz5vDeW'
+        document_ids['Bank Information Request Form - '] = '1hOZUQ3r1dPi6_nOrHZTioQ4MlT8YXGox'
+        document_ids['New Owner & Property Onboarding Template - '] = '1tmJYZKIKYtdfVe6q6AH1TeXNtyDOx5vdapk7TJeRqa4'
 
-        # Save the new copy ID
-        new_copies_ids.append(copied_file['id'])
+        owner_folder = onboarding_object.owner_folder_id
 
-    # Handle post-copy logic or response
-    print("Copied Files IDs:", new_copies_ids)
-    return redirect('dashboard')
+        for name,doc_id in document_ids.items():
+            copy_metadata = {
+                'name': f'{name}{owner}',
+                'parents': [owner_folder] 
+            }
+
+            # Create a copy of the document/form
+            copied_file = drive_service.files().copy(
+                fileId=doc_id,
+                body=copy_metadata,
+                fields='id'
+            ).execute()
+
+            # Save the new copy ID
+            new_copies_ids.append(copied_file['id'])
+
+        # Handle post-copy logic or response
+        print("Copied Files IDs:", new_copies_ids)
 
 def dashboard_step_four_view(request):
     if request.method == "POST":  # Ensure the method check is correctly capitalized
